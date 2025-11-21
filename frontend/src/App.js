ff7bec966887ee170594ad3f5ba3014c09f611a2
RAJALAKSHMI66 <rajalakshmiraveendran33@gmail.com>
2025-11-21 22:37:57 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AirAwareDashboard from './components/AirAwareDashboard';
+
+function App() {
+  return (
+    <div>
+      <AirAwareDashboard />
+    </div>
+  );
+}
+
+export default App;
